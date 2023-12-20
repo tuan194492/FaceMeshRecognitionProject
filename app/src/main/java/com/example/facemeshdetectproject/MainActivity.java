aa8f945275d10777493ffc74bd74753503b8657e
tuan194492 <tuan1216302001@gmail.com>
2023-12-21 00:14:46 +0700

    First commit

---
+++ app/src/main/java/com/example/facemeshdetectproject/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.facemeshdetectproject;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
+public class MainActivity extends AppCompatActivity implements ImageAnalysis.Analyzer {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private ImageButton record, flipCamera;
+    private PreviewView previewView;
+    private ImageCapture imageCapture;
+    private ImageAnalysis imageAnalysis;
+    Camera camera;
+    ProcessCameraProvider cameraProvider = null;
+
+    int cameraFacing = CameraSelector.LENS_FACING_FRONT;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        record = findViewById(R.id.start_record);
+        flipCamera = findViewById(R.id.flip_camera);
+
+        record.setOnClickListener(this::onClickRecord);
+        flipCamera.setOnClickListener(this::onClickFlipCamera);
+        previewView = findViewById(R.id.preview);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderFuture.get();
+                System.out.println("Tuan NQ");
+                startCameraX(cameraProvider);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }, getExecutor());
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{android.Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(cameraFacing).build();
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build();
+        imageAnalysis = new ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+        imageAnalysis.setAnalyzer(getExecutor(), this::analyze);
+        camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+    }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void onClickFlipCamera(View view) {
+        if (cameraFacing == CameraSelector.LENS_FACING_BACK) {
+            cameraFacing = CameraSelector.LENS_FACING_FRONT;
+        } else {
+            cameraFacing = CameraSelector.LENS_FACING_BACK;
+        }
+        startCameraX(cameraProvider);
+    }
+
+    private void onClickRecord(View view) {
+    }
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        Log.d("TUAN", "Got image at " + image.getImageInfo() );
+        System.out.println("Tuan NQ" + "Got image at " + image.getImageInfo().getTimestamp());
+        image.close();
+
+    }
+}
