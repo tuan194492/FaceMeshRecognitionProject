6755b9b6beeea59418f0680804f89a36f56f8002
trongdat-123 <trongdatdo363@gmail.com>
2023-12-27 23:25:30 +0700

    feat: register

--- app/src/main/java/com/example/facemeshdetectproject/RegisterActivity.java
+++ app/src/main/java/com/example/facemeshdetectproject/RegisterActivity.java
@@ -1,18 +1,47 @@
 package com.example.facemeshdetectproject;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Toast;
-
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private Button btnSave;
+    private String faceMeshPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_input_name);
+
+        editTextName = findViewById(R.id.editTextName);
+        btnSave = findViewById(R.id.btnSave);
+
         Intent intent = getIntent();
-        System.out.println("HAHA" + intent.getExtras().get("faceMeshList"));
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            String valueShow = bundle.getString("faceMeshPoints", "");
+            faceMeshPoints = valueShow;
+        }
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData() {
+        String name = editTextName.getText().toString();
+
+        // Lưu dữ liệu vào SQLite
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        dbHelper.saveFaceData(name, faceMeshPoints);
+
+        finish();
     }
 }
